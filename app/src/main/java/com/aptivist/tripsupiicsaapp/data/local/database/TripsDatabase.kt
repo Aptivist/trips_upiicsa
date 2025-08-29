8a66da766469dcfcd2a4a3595654715d67580cc3
chekostyle <cheko@acidbeat.net>
2025-08-29 17:57:03 -0600

    Updates database schema to version 3
    Updates the database schema to version 3, including new entities and relationships.
    
    Adds auto-migration from versions 1 and 2.
    Updates TripPhotoEntity to remove unnecessary indexing.

--- app/src/main/java/com/aptivist/tripsupiicsaapp/data/local/database/TripsDatabase.kt
+++ app/src/main/java/com/aptivist/tripsupiicsaapp/data/local/database/TripsDatabase.kt
@@ -25,10 +25,11 @@ import com.aptivist.tripsupiicsaapp.data.local.database.entities.TripPhotoEntity
         JournalEntryEntity::class,
         CheckListEntryEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = true,
     autoMigrations = [
-        AutoMigration(from = 1, to = 2)
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3)
     ]
 )
 abstract class TripsDatabase : RoomDatabase() {
